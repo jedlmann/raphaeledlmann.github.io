422ccaac0cee1e37d38c1a1d727a9594fd62defe
Raphael Edlmann <r.edlmann@gmail.com>
2020-11-20 16:16:41 +0000

    php
    contact

--- contact.php
+++ contact.php
@@ -6,14 +6,14 @@ $message = $_POST['message'];
 
 $email_from = 'r.edlmannl@gmail.com';
 $email_subject = "New Form Submission";
-$email_body = "User Name; $name.\n".
+$email_body = "User Name: $name.\n".
               "User Email: $visitor_email.\n".
                 "User Message: $message.\n";
 
 $to = "r.edlmann@gmail.com";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
+mail($to, $email_subject, $email_body, $headers);
 
 header("Location: index.html");
 
