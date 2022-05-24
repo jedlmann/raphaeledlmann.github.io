fd383d3299cda337aae5af1f78dfff99ca3f8133
rafattacks <r.edlmann@gmail.com>
2022-05-24 21:00:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Game Developer", "QA Tester", "Web Development"],
+    strings: ["Game Development", "QA Testing", "Web Development"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
